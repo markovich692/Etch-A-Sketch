cab34520682055b629eac8dd995df59e354f89e6
Marc Diallo <mark.devguru@outlook.com>
2025-11-18 10:31:55 -0600

    Add createGrid function to generate N grid cells inside grid-container

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+const gridContainer = document.querySelector(".grid-container");
+const gridCell = document.createElement("div");
+
+const createGrid = function (size) {
+  //Create a grid of n grid cells
+  for (let i = 0; i < size * size; i++) {
+    const cell = gridCell.cloneNode();
+
+    cell.classList.add("grid-cell");
+    gridContainer.appendChild(cell);
+  }
+};
+
+createGrid(16);
+
+console.log(gridContainer);
