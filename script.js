ed1938dcd6d2a57f60eb57ec113e7cb42e03826a
Marc Diallo <mark.devguru@outlook.com>
2025-11-18 13:34:41 -0600

    Set and resize grid cells based on container size and grid dimensions
    - Calculates width and height of each grid cell relative to container size, root font size, and total grid size.
    - Ensures cells evenly fill the grid without overflowing.

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 const gridContainer = document.querySelector(".grid-container");
 const gridCell = document.createElement("div");
+let cell;
 
+const setGridCellSize = function (computedStyleWidthPx, computedStyleHeightPx, rootFontSize, size) {
+  document.querySelectorAll(".grid-cell").forEach(function (el) {
+    const gridCellWidthRem = computedStyleWidthPx / size / rootFontSize;
+    const gridCellHeightRem = computedStyleHeightPx / size / rootFontSize;
+
+    el.style.width = gridCellWidthRem + "rem";
+    el.style.height = gridCellHeightRem + "rem";
+  });
+};
+
+const resizeGridCells = function (size) {
+  //Get computed gridContainer width and height
+  const computedStyleWidthPx = parseFloat(getComputedStyle(gridContainer).width);
+  const computedStyleHeightPx = parseFloat(getComputedStyle(gridContainer).height);
+
+  //Get computed root font size
+  const rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+  //Set grid cell width and height
+
+  setGridCellSize(computedStyleWidthPx, computedStyleHeightPx, rootFontSize, size);
+};
+
+//Create a grid of n x n size
 const createGrid = function (size) {
-  //Create a grid of n grid cells
   for (let i = 0; i < size * size; i++) {
-    const cell = gridCell.cloneNode();
-
+    cell = gridCell.cloneNode();
     cell.classList.add("grid-cell");
     gridContainer.appendChild(cell);
   }
+
+  resizeGridCells(size);
 };
 
-createGrid(16);
-
-console.log(gridContainer);
+createGrid(99);
